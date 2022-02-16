f16db25adbe9c1a53983a8f301ba3234e34e08f9
tranqui.eth <thinkandchill11@gmail.com>
2022-02-16 02:02:57 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+//const { evaluate } = require('mathjs')
+
+//let check = evaluate(1.2 * (2 + 4.5));
+
+//console.log(check);
+
+let display = document.getElementById('results');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'AC':
+                //debugger
+            //console.log("hello world");
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
+
+//const { evaluate } = require('mathjs');
